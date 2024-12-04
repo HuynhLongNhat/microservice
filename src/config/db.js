9c386b586dd989f8bba8b3ae1224586832cbff83
Long Nhat <nhathuynh227@gmail.com>
2024-12-04 13:45:33 +0700

    config db

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,18 @@
 import mysql from "mysql2";
 
-const connection = mysql.createConnection({
+const dbConnect = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "admin",
   database: "th2",
 });
 
-function connect() {
-  connection.connect((err) => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      return;
-    }
-    console.log("Connected to database!");
-  });
-}
+dbConnect.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to database!");
+});
 
-const dbConnect = { connect, connection };
 export default dbConnect;
